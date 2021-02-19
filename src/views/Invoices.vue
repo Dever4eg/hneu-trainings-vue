17a1510f1bf5f8885595146227d5b490efc6b3c9
Aleksandr Drobot <alex.drobot22@gmail.com>
2021-02-19 02:38:29 +0200

    Added filters for invoices, added fetch invoices functionality

--- src/views/Invoices.vue
+++ src/views/Invoices.vue
@@ -1,8 +1,8 @@
 <template>
   <v-layout column>
-    <div class="d-flex align-center">
-      <h5 class="text-h5 font-weight-light mt-3 mb-4">Поставки</h5>
-      <InvoiceCreateFormDialog>
+    <div class="d-flex mt-3 mb-4">
+      <h5 class="text-h5 font-weight-light">Поставки</h5>
+      <InvoiceCreateFormDialog @created="fetchInvoices">
         <template v-slot:activator="{ on, attrs }">
           <v-btn
             class="mb-2 ml-4 mx-2"
@@ -19,6 +19,94 @@
       </InvoiceCreateFormDialog>
     </div>
 
+    <div class="d-flex justify-space-between">
+      <div>
+        <v-select
+          :items="suppliers"
+          :loading="fetchingSuppliers"
+          label="Назва постачальника"
+          solo
+          clearable
+          @change="filterInvoices"
+        ></v-select>
+      </div>
+
+      <div class="d-flex">
+        <v-dialog
+          ref="firstDate"
+          v-model="firstDateModal"
+          :return-value.sync="firstDate"
+          persistent
+          width="290px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="firstDate"
+              class="mx-5"
+              label="Від"
+              solo
+              readonly
+              clearable
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="firstDate" scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey darken-4" @click="firstDateModal = false">
+              Cancel
+            </v-btn>
+            <v-btn
+              text
+              color="grey darken-4"
+              @click="
+                $refs.firstDate.save(firstDate);
+                fetchInvoices();
+              "
+            >
+              OK
+            </v-btn>
+          </v-date-picker>
+        </v-dialog>
+
+        <v-dialog
+          ref="lastDate"
+          v-model="lastDateModal"
+          :return-value.sync="lastDate"
+          persistent
+          width="290px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="lastDate"
+              label="До"
+              solo
+              readonly
+              clearable
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker v-model="lastDate" scrollable>
+            <v-spacer></v-spacer>
+            <v-btn text color="grey darken-4" @click="lastDateModal = false">
+              Cancel
+            </v-btn>
+            <v-btn
+              text
+              color="grey darken-4"
+              @click="
+                $refs.lastDate.save(lastDate);
+                fetchInvoices();
+              "
+            >
+              OK
+            </v-btn>
+          </v-date-picker>
+        </v-dialog>
+      </div>
+    </div>
+
     <v-layout v-if="loading" class="justify-center">
       <v-progress-circular
         :size="50"
@@ -27,16 +115,25 @@
       ></v-progress-circular>
     </v-layout>
 
+    <v-layout v-if="filteredInvoices && !filteredInvoices.length">
+      <p class="text--secondary font-weight-regular">Поставок не знайдено</p>
+    </v-layout>
+
     <v-card
-      v-for="{ id, supplier, products, created_at: createdAt } in invoices"
+      v-for="{
+        id,
+        date,
+        products,
+        supplier_name: supplierName
+      } in filteredInvoices"
       :key="id"
       class="my-3 px-3"
     >
       <v-card-text class="d-flex justify-space-between mt-3">
-        <span class="text--primary font-weight-medium">{{
-          supplier.name
-        }}</span>
-        <span>{{ createdAt | moment("DD.MM.YYYY HH:mm") }}</span>
+        <span class="text--primary font-weight-medium">
+          {{ supplierName }}
+        </span>
+        <span>{{ date | moment("DD.MM.YYYY HH:mm") }}</span>
       </v-card-text>
       <v-card-text>
         <v-simple-table>
@@ -56,16 +153,14 @@
                 <td>{{ product.name }}</td>
                 <td>${{ product.price }}</td>
                 <td>{{ product.count }}</td>
-                <td>${{ product.price * product.count }}</td>
+                <td>${{ (product.price * product.count).toFixed(2) }}</td>
               </tr>
             </tbody>
           </template>
         </v-simple-table>
       </v-card-text>
       <v-card-text class="d-flex justify-end text--primary font-weight-medium">
-        <p>
-          Всього: ${{ products.reduce((total, item) => total + (item.price * item.count), 0) }}
-        </p>
+        <p>Всього: ${{ computeInvoiceSum(products) }}</p>
       </v-card-text>
     </v-card>
   </v-layout>
@@ -73,6 +168,7 @@
 
 <script>
 import InvoiceCreateFormDialog from "@/components/Invoices/InvoiceCreateFormDialog";
+import { getAuthHeader } from "../helpers/auth";
 
 export default {
   name: "Invoices",
@@ -80,39 +176,62 @@ export default {
   data() {
     return {
       loading: false,
-      invoices: []
+      fetchingSuppliers: false,
+      suppliers: [],
+      invoices: [],
+      filteredInvoices: [],
+      firstDateModal: null,
+      lastDateModal: null,
+      firstDate: "",
+      lastDate: ""
     };
   },
   mounted() {
+    this.fetchSuppliers();
     this.fetchInvoices();
   },
   methods: {
+    fetchSuppliers() {
+      this.fetchingSuppliers = true;
+      // eslint-disable-next-line no-undef
+      axios
+        .get("/suppliers", { headers: { ...getAuthHeader() } })
+        .then(response => {
+          const { suppliers } = response.data.data;
+
+          this.suppliers = suppliers.map(({ name }) => name);
+        })
+        .finally(() => {
+          this.fetchingSuppliers = false;
+        });
+    },
     fetchInvoices() {
       this.loading = true;
-      setTimeout(() => {
-        this.invoices = [
-          {
-            id: 1,
-            supplier: {
-              name: "Bob"
-            },
-            products: [
-              {
-                count: 5,
-                name: "Steering Wheel",
-                price: 300
-              },
-              {
-                count: 10,
-                name: "Seat Belt",
-                price: 150
-              }
-            ],
-            created_at: "16.02.2021 15:15"
-          }
-        ];
-        this.loading = false;
-      }, 300);
+      this.filteredInvoices = [];
+      // eslint-disable-next-line no-undef
+      axios
+        .get(`/invoices?first_date=${this.firstDate}&last_date=${this.lastDate}`, { headers: { ...getAuthHeader() } })
+        .then(response => {
+          this.invoices = response.data.data;
+          this.filteredInvoices = this.invoices;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    filterInvoices(supplierName) {
+      if (!supplierName) {
+        this.filteredInvoices = this.invoices;
+      } else {
+        this.filteredInvoices = this.invoices.filter(
+          ({ supplier_name }) => supplier_name === supplierName
+        );
+      }
+    },
+    computeInvoiceSum(products) {
+      return products
+        .reduce((total, item) => total + item.price * item.count, 0)
+        .toFixed(2);
     }
   }
 };
